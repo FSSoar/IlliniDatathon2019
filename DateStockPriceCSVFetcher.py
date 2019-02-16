1e136ff2c6d10b7fd913fc9815dd4d2a9c86c7e5
Sudesh Sahu <sahu.k.sudesh@gmail.com>
2019-02-15 23:11:47 -0600

    added a simple daily stock price fetcher for a given company

--- DateStockPriceCSVFetcher.py
+++ DateStockPriceCSVFetcher.py
@@ -1,9 +1,21 @@
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
+import requests
+import os
 
-def getCSVData():
-    ts = TimeSeries(key='VRC35Q4ME9BN01BF', output_format='csv')
-    data, meta_data = ts.get_daily(symbol="BAYRY", outputsize='compact')
-    pprint(data)
+def getCSVData(function, symbol):
+    
+    url = "https://www.alphavantage.co/query?function="+function+"&symbol="+symbol+"&datatype=csv"+"&apikey=UMB1WO6HDSOV980F"
+    filename = symbol + ".csv"
+    if not os.path.isfile(filename):
+        print('Downloading File')
+        response = requests.get(url)
+        # Check if the response is ok (200)
+        if response.status_code == 200:
+            # Open file and write the content
+            with open(filename, 'wb') as file:
+                # A chunk of 128 bytes
+                for chunk in response:
+                    file.write(chunk)
+    else:
+        print('File exists')
 
-getCSVData()
+getCSVData("TIME_SERIES_DAILY", "BAYZF")
